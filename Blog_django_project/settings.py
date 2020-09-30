f7cfe39c5561f58bbd52907700cbe96398e71e80
abakums <abakumovs882@gmail.com>
2020-09-30 19:00:47 +0300

    Sqlite database

--- Blog_django_project/settings.py
+++ Blog_django_project/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'Blog_django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blog_django_project_db',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
